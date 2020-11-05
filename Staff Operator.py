904aca9a6e9cbb8ce36ad97358149f832b8404f5
YuvrajsinhZala <45811942+YuvrajsinhZala@users.noreply.github.com>
2020-11-05 10:34:59 +0530

    Update Staff Operator.py

--- Staff Operator.py
+++ Staff Operator.py
@@ -8,9 +8,12 @@ from tkinter import *
 from turtle import *
 import random
 
-conn = sqlite3.connect('Yuvraj-leaveDb.db')
+conn = sqlite3.connect('Yuvraj.db')
 cur = conn.cursor()
 
+#conn.execute("CREATE TABLE balance (employee_id text,sickleave int,maternityleave int,emergencyleave int)")
+#conn.execute("CREATE TABLE status (leave_id int,employee_id text,leave text,Date1 text,Date2 text,days int,status text)")
+#conn.execute('''CREATE TABLE employee (employee_id text,Name text,ContactNumber text,Password text)''')
 
 def AdminLogin():
     message = "Enter Username and Password"
@@ -91,6 +94,34 @@ def EmployeeAllInformationWindow():
     txt.pack()
 
 
+def WindowStatus():
+    StatusWindow = Toplevel()
+    label_1 = Label(StatusWindow, text="Employee ID=", fg="blue", justify=LEFT, font=("Calibri", 16))
+    label_2 = Label(StatusWindow, text=leaveStatus[1], font=("Calibri", 16))
+    label_3 = Label(StatusWindow, text="Type=", fg="blue", font=("Calibri", 16), justify=LEFT)
+    label_4 = Label(StatusWindow, text=leaveStatus[2], font=("Calibri", 16))
+    label_5 = Label(StatusWindow, text="start=", fg="blue", font=("Calibri", 16), justify=LEFT)
+    label_6 = Label(StatusWindow, text=leaveStatus[3], font=("Calibri", 16))
+    label_7 = Label(StatusWindow, text="end=", fg="blue", font=("Calibri", 16), justify=LEFT)
+    label_8 = Label(StatusWindow, text=leaveStatus[4], font=("Calibri", 16))
+    label_9 = Label(StatusWindow, text="Status:", fg="blue", font=("Calibri", 16), justify=LEFT)
+    label_10 = Label(StatusWindow, text=leaveStatus[6], font=("Calibri", 16))
+    label_11 = Label(StatusWindow, text="leave_id:", fg="blue", font=("Calibri", 16), justify=LEFT)
+    label_12 = Label(StatusWindow, text=leaveStatus[0], font=("Calibri", 16))
+    label_11.grid(row=0, column=0)
+    label_12.grid(row=0, column=1)
+    label_1.grid(row=1, column=0)
+    label_2.grid(row=1, column=1)
+    label_3.grid(row=2, column=0)
+    label_4.grid(row=2, column=1)
+    label_5.grid(row=3, column=0)
+    label_6.grid(row=3, column=1)
+    label_7.grid(row=4, column=0)
+    label_8.grid(row=4, column=1)
+    label_9.grid(row=5, column=0)
+    label_10.grid(row=5, column=1)
+
+
 def balance():
     global login
     check = (login,)
@@ -102,9 +133,96 @@ def balance():
     WindowBalance()
 
 
+def WindowBalance():
+    balanceWindow = Toplevel()
+    label_1 = Label(balanceWindow, text="Employee ID=", fg="blue", justify=LEFT, font=("Calibri", 16))
+    label_2 = Label(balanceWindow, text=balanced[0], font=("Calibri", 16))
+    label_3 = Label(balanceWindow, text="Sick Leave=", fg="blue", font=("Calibri", 16), justify=LEFT)
+    label_4 = Label(balanceWindow, text=balanced[1], font=("Calibri", 16))
+    label_5 = Label(balanceWindow, text="Maternity Leave=", fg="blue", font=("Calibri", 16), justify=LEFT)
+    label_6 = Label(balanceWindow, text=balanced[2], font=("Calibri", 16))
+    label_7 = Label(balanceWindow, text="Emergency Leave=", fg="blue", font=("Calibri", 16), justify=LEFT)
+    label_8 = Label(balanceWindow, text=balanced[3], font=("Calibri", 16))
+    label_1.grid(row=0, column=0)
+    label_2.grid(row=0, column=1)
+    label_3.grid(row=1, column=0)
+    label_4.grid(row=1, column=1)
+    label_5.grid(row=2, column=0)
+    label_6.grid(row=2, column=1)
+    label_7.grid(row=3, column=0)
+    label_8.grid(row=3, column=1)
+
+
+def apply():
+    message = "Enter the following details "
+    title = "Leave Apply"
+    fieldNames = ["Employee ID", "From", "To", "days"]
+    fieldValues = []
+    fieldValues = multenterbox(message, title, fieldNames)
+    message1 = "Select type of leave"
+    title1 = "Type of leave"
+    choices = ["Sick leave", "Maternity leave", "Emergency leave"]
+    choice = choicebox(message1, title1, choices)
+    leaveid = random.randint(1, 1000)
+
+    conn.execute("INSERT INTO status(leave_id,employee_id,leave,Date1,Date2,days,status) VALUES (?,?,?,?,?,?,?)",
+                 (leaveid, fieldValues[0], choice, fieldValues[1], fieldValues[2], fieldValues[3], "Pending"))
+    conn.commit()
+
 def LeaveApproval():
-    exit;
-    
+    message = "Enter leave_id"
+    title = "leave approval"
+    fieldNames = ["Leave_id"]
+    fieldValues = []
+    fieldValues = multenterbox(message, title, fieldNames)
+    message1 = "Approve/Deny"
+    title1 = "leave approval"
+    choices = ["approve", "deny"]
+    choice = choicebox(message1, title1, choices)
+
+    conn.execute("UPDATE status SET status = ? WHERE leave_id= ?", (choice, fieldValues[0]))
+    conn.commit()
+
+    if choice == 'approve':
+        print(0)
+        cur.execute("SELECT leave FROM status WHERE leave_id=?", (fieldValues[0],))
+        row = cur.fetchall()
+        col = row
+
+        for row in conn.execute("SELECT employee_id FROM status WHERE leave_id=?", (fieldValues[0],)):
+            print(2)
+            exampleId = row[0]
+
+        for row in conn.execute("SELECT days FROM status WHERE leave_id=?", (fieldValues[0],)):
+            print(2)
+            exampleDays = row[0]
+
+        for row in conn.execute("SELECT sickleave from balance where employee_id=?", (exampleId,)):
+            balance = row[0]
+            print(balance)
+
+        for row in conn.execute("SELECT maternityleave from balance where employee_id=?", (exampleId,)):
+            balance1 = row[0]
+            print(balance1)
+
+        for row in conn.execute("SELECT emergencyleave from balance where employee_id=?", (exampleId,)):
+            balance2 = row[0]
+            print(balance2)
+
+        if (col[0] == ('sickleave',)):
+            print(3)
+            conn.execute("UPDATE balance SET sickleave =? WHERE employee_id= ?", ((balance - exampleDays), (exampleId)))
+
+        if (col[0] == ('maternityleave',)):
+            print(3)
+            conn.execute("UPDATE balance SET maternityleave =? WHERE employee_id= ?", ((balance1 - exampleDays), (exampleId)))
+
+        if (col[0] == ('emergencyleave',)):
+            print(3)
+            conn.execute("UPDATE balance SET emergencyleave =? WHERE employee_id= ?", ((balance2 - exampleDays), (exampleId)))
+
+
+
 def leavelist():
     leavelistwindow = Toplevel()
     txt = Text(leavelistwindow)
@@ -146,38 +264,42 @@ def EmployeeLoginWindow():
     Background_Label = Label(LoginWindow, image=filename)
     Background_Label.place(x=0, y=0, relwidth=1, relheight=1)
 
-    informationEmployee = Button(LoginWindow, text='Employee information', command=EmployeeInformationWindow, bd=12, relief=GROOVE, fg="green", bg="#ffffb3",
+    informationEmployee = Button(LoginWindow, text='Employee information', command=EmployeeInformationWindow, bd=12, relief=GROOVE, fg="blue", bg="azure",
                       font=("Calibri", 36, "bold"), pady=3)
     informationEmployee['font'] = BtnFont
     informationEmployee.pack(fill=X)
 
-    submit = Button(LoginWindow, text='Submit Leave', command=apply, bd=12, relief=GROOVE, fg="white", bg="#ff0003",
+    submit = Button(LoginWindow, text='Submit Leave', command=apply, bd=12, relief=GROOVE, fg="blue", bg="azure",
                       font=("Calibri", 36, "bold"), pady=3)
     submit['font'] = BtnFont
     submit.pack(fill=X)
 
-    LeaveBalance = Button(LoginWindow, text='Leave Balance', command=balance, bd=12, relief=GROOVE, fg="white", bg="#ff0003",
+    LeaveBalance = Button(LoginWindow, text='Leave Balance', command=balance, bd=12, relief=GROOVE, fg="blue", bg="azure",
                       font=("Calibri", 36, "bold"), pady=3)
     LeaveBalance['font'] = BtnFont
     LeaveBalance.pack(fill=X)
 
-    LeaveApplicationStatus = Button(LoginWindow, text='Last leave status', command=EmployeeLeaveStatus, bd=12, relief=GROOVE, fg="white", bg="#ff0003",
+    LeaveApplicationStatus = Button(LoginWindow, text='Last leave status', command=EmployeeLeaveStatus, bd=12, relief=GROOVE, fg="blue", bg="azure",
                       font=("Calibri", 36, "bold"), pady=3)
     LeaveApplicationStatus['font'] = BtnFont
     LeaveApplicationStatus.pack(fill=X)
 
-    AllLeaveStatus = Button(LoginWindow, text='All leave status', command=EmployeeAllStatus, bd=12, relief=GROOVE, fg="white", bg="#ff0003",
+    AllLeaveStatus = Button(LoginWindow, text='All leave status', command=EmployeeAllStatus, bd=12, relief=GROOVE, fg="blue", bg="azure",
                       font=("Calibri", 36, "bold"), pady=3)
     AllLeaveStatus['font'] = BtnFont
     AllLeaveStatus.pack(fill=X)
 
 
-    LogoutBtn = Button(LoginWindow, text='Logout', bd=12, relief=GROOVE, fg="red", bg="#ffffb3",
+    LogoutBtn = Button(LoginWindow, text='Logout', bd=12, relief=GROOVE, fg="red", bg="wheat",
                       font=("Calibri", 36, "bold"), pady=3, command=Employeelogout)
     LogoutBtn['font'] = BtnFont
     LogoutBtn.pack(fill=X)
 
     informationEmployee.pack()
+    submit.pack()
+    LeaveBalance.pack()
+    LeaveApplicationStatus.pack()
+    AllLeaveStatus.pack()
     LogoutBtn.pack()
     ExitBtn.pack()
 
@@ -189,64 +311,66 @@ def adminwindow():
     Background_Label = Label(adminmainwindow, image=filename)
 
     Background_Label.place(x=0, y=0, relwidth=1, relheight=1)
-    informationEmployee = Button(adminmainwindow, text='All Employee information', command=EmployeeAllInformationWindow, bd=12, relief=GROOVE, fg="blue", bg="#ffffb3",
+    informationEmployee = Button(adminmainwindow, text='All Employee information', command=EmployeeAllInformationWindow, bd=12, relief=GROOVE, fg="blue", bg="azure",
                       font=("Calibri", 36, "bold"), pady=3)
     informationEmployee['font'] = BtnFont
     informationEmployee.pack(fill=X)
 
 
 
-    LeaveListButton = Button(adminmainwindow, text='Leave approval list', command=leavelist, bd=12, relief=GROOVE, fg="white", bg="#ff0003",
+    LeaveListButton = Button(adminmainwindow, text='Leave approval list', command=leavelist, bd=12, relief=GROOVE, fg="blue", bg="azure",
                       font=("Calibri", 36, "bold"), pady=3)
     LeaveListButton['font'] = BtnFont
     LeaveListButton.pack(fill=X)
 
-    ApprovalButton = Button(adminmainwindow, text='Approve leave', command=LeaveApproval, bd=12, relief=GROOVE, fg="white", bg="#ff0003",
+    ApprovalButton = Button(adminmainwindow, text='Approve leave', command=LeaveApproval, bd=12, relief=GROOVE, fg="blue", bg="azure",
                       font=("Calibri", 36, "bold"), pady=3)
     ApprovalButton['font'] = BtnFont
     ApprovalButton.pack(fill=X)
 
     LogoutBtn = Button(adminmainwindow, text='Logout', command=adminmainwindow.destroy, bd=12, relief=GROOVE, fg="red",
-                     bg="#ffffb3",
+                     bg="wheat",
                      font=("Calibri", 36, "bold"), pady=3)
     LogoutBtn['font'] = BtnFont
     LogoutBtn.pack(fill=X)
 
     informationEmployee.pack()
+    LeaveListButton.pack()
+    ApprovalButton.pack()
     ExitBtn.pack()
 
 
 root = Tk()
 root.wm_attributes('-fullscreen', '1')
-root.title("Staff Operator - YZ")
+root.title("Staff operator")
 root.iconbitmap(default='leavelogo.ico')
 filename = PhotoImage(file="background.gif")
 background_label = Label(root, image=filename)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 BtnFont = Font(family='Calibri(Body)', size=20)
-MainLabel = Label(root, text="Staff Operator - YZ", bd=12, relief=GROOVE, fg="White", bg="green",
+MainLabel = Label(root, text="Staff operator", bd=12, relief=GROOVE, fg="wheat", bg="gray",
                       font=("Calibri", 36, "bold"), pady=3)
 MainLabel.pack(fill=X)
 im = PhotoImage(file='login.gif')
 
-AdminLgnBtn = Button(root, text='Admin login',  bd=12, relief=GROOVE, fg="blue", bg="#ffffb3",
+AdminLgnBtn = Button(root, text='Admin login',  bd=12, relief=GROOVE, fg="blue", bg="azure",
                       font=("Calibri", 36, "bold"), pady=3, command=AdminLogin)
 AdminLgnBtn['font'] = BtnFont
 AdminLgnBtn.pack(fill=X)
 
 
-LoginBtn = Button(root, text='Employee login', bd=12, relief=GROOVE, fg="blue", bg="#ffffb3",
+LoginBtn = Button(root, text='Employee login', bd=12, relief=GROOVE, fg="blue", bg="azure",
                       font=("Calibri", 36, "bold"), pady=3, command=EmployeeLogin)
 LoginBtn['font'] = BtnFont
 LoginBtn.pack(fill=X)
 
 
-EmployeeRegistration = Button(root, text='Employee registration', command=registration, bd=12, relief=GROOVE, fg="blue", bg="#ffffb3",
+EmployeeRegistration = Button(root, text='Employee registration', command=registration, bd=12, relief=GROOVE, fg="blue", bg="azure",
                       font=("Calibri", 36, "bold"), pady=3)
 EmployeeRegistration['font'] = BtnFont
 EmployeeRegistration.pack(fill=X)
 
-ExitBtn = Button(root, text='Exit', command=root.destroy, bd=12, relief=GROOVE, fg="red", bg="#ffffb3",
+ExitBtn = Button(root, text='Exit', command=root.destroy, bd=12, relief=GROOVE, fg="red", bg="wheat",
                       font=("Calibri", 36, "bold"), pady=3)
 ExitBtn['font'] = BtnFont
 ExitBtn.pack(fill=X)
